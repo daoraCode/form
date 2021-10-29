2ef89e671e6c95adfe98eca89fd1ff6a0c253463
daoraCode <severin.mboukou@gmail.com>
2021-10-29 17:31:30 +0200

    update App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import { render } from 'react-dom';
-
-// var regex = /[\w\.]+@([\w-]+\.)+[\w-]{2,4}$/
+// import { render } from 'react-dom';
 
 class App extends React.Component {
   //! constructor to initialize states objects
@@ -21,17 +19,29 @@ class App extends React.Component {
 
     //! binding
     this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  // get rid of refreshing page
+  handleSubmit(e) {
+    alert('A name was submitted: ' + this.state.email);
+    e.preventDefault();
   }
 
   //! custom methods or React functions
   handleEmailChange(e) {
-    console.log('handleEmailChange');
-    this.setState({ value: e.target.value });
+    this.setState({ email: e.target.value });
+    console.log(e.target.value);
+    var regex = /[\w\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(e.target.value);
+    if(regex) {
+      this.setState({ passwordValid: true}) 
+    }
   }
 
-  handleEmailChange(e) {
-    console.log('handleEmailChange');
-    this.setState({ value: e.target.value });
+  handlePasswordChange(e) {
+    this.setState({ password: e.target.value });
+    console.log(e.target.value);
   }
 
   render() {
@@ -39,23 +49,37 @@ class App extends React.Component {
     // const {} = this.props;
 
     //! states
-    const { email } = this.state;
+    const { email, password } = this.state;
 
     return (
-      <div className='container mx-5 my-5'>
-        <form>
-        <h1>Login</h1>
-          <label for='exampleFormControlInput1' className='form-label'>
-            Enter email...
+      <div className='container mx-3 my-5 px-5 py-5 justify-content-center'>
+        <form onSubmit={this.handleSubmit} autoComplete="off">
+          <h1>Login</h1>
+          <label className='form-label my-3'>
+            Email adress
           </label>
           <input
-            type='email'
-            className='form-control'
+            type='text'
+            className='form-control my-3'
             id='exampleFormControlInput1'
-            placeholder='name@example.com'
-            value={email} 
+            placeholder='Enter email...'
+            value={email}
             onChange={this.handleEmailChange}
+            required
           ></input>
+          <label className='form-label my-3'>
+            Password
+          </label>
+          <input
+            type='text'
+            className='form-control my-3'
+            id='exampleFormControlInput1'
+            placeholder='Enter password...'
+            value={password}
+            onChange={this.handlePasswordChange}
+            required
+          ></input>
+          <button type="submit" className="btn btn-primary my-3">Submit</button>
         </form>
       </div>
     );
